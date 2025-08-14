10165a8e46af50ff8da0ed717ffda3728af9a411
Ilham <ilham.hipster@gmail.com>
2025-08-14 22:49:20 +0500

    minor fixes

--- app/(checkout)/layout.tsx
+++ app/(checkout)/layout.tsx
@@ -8,11 +8,11 @@ export const metadata: Metadata = {
 
 export default function CheckoutLayout({
     children,
-    modal,
-}: Readonly<{
+    modal
+}: {
     children: React.ReactNode;
     modal: React.ReactNode;
-}>) {
+}) {
     return (
         <main className="min-h-screen bg-[#F4F1EE]">
             <Header hasCart={false} hasSearch={false} />
